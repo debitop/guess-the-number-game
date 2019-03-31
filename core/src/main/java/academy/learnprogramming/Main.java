d9d384997273b82b95ab69625f90320efdcf5e85
Aleksandr Kochkurkin <mailto:89636617290@mail.ru>
2019-03-31 23:55:57 +0200

    commit

--- core/src/main/java/academy/learnprogramming/Main.java
+++ core/src/main/java/academy/learnprogramming/Main.java
@@ -3,21 +3,20 @@ package academy.learnprogramming;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
     private static final Logger log = LoggerFactory.getLogger(Main.class);
 
-    private static final String CONFIG_LOCATION = "beans.xml";
 
     public static void main(String[] args) {
         log.info("Guess The Number Game");
 
         // create context (container)
-        ConfigurableApplicationContext context = new ClassPathXmlApplicationContext(CONFIG_LOCATION);
+        ConfigurableApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 
         // get number generator bean from context (container)
-        NumberGenerator numberGenerator = context.getBean("numberGenerator", NumberGenerator.class);
+        NumberGenerator numberGenerator = context.getBean(NumberGenerator.class);
 
         // call method next() to get a random number
         int number = numberGenerator.next();
@@ -26,7 +25,13 @@ public class Main {
         log.info("number = {}", number);
 
         // get game bean from context (container)
-        Game game = context.getBean(Game.class);
+        //     Game game = context.getBean(Game.class);
+
+        // get message generator bean from context (container)
+        MessageGenerator messageGenerator = context.getBean(MessageGenerator.class);
+
+        log.info("getMainMessage= {}", messageGenerator.getMainMessage());
+        log.info("getResultMessage= {}", messageGenerator.getResultMessage());
 
         // close context (container)
         context.close();
